ee817a3ca07c1704904f8924c0b9eb0fb7a7ebd3
Kstu24 <Kistewart419@gmail.com>
2020-12-02 21:08:14 -0500

    MVP

--- hashtables/ex4/ex4.py
+++ hashtables/ex4/ex4.py
@@ -3,6 +3,14 @@ def has_negatives(a):
     YOUR CODE HERE
     """
     # Your code here
+    result = []
+    new_dict = {}
+
+    for num in a:
+        new_dict[num] = num
+
+        if num != 0 and - num in new_dict:
+            result.append(abs(num))
 
     return result
 
